608b3da54dfa13f54c3bbbb8e24e8174f596cca4
Dutch <dutch@unviral.com>
2018-02-14 14:25:56 +0800

    Init new nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="section">
+    <h1>Hello, World!</h1>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
