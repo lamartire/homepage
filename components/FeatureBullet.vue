7e507cc3d4901330c93c5b5dff43d9fa43f84a17
Dutch <dutch@unviral.com>
2018-02-14 17:31:55 +0800

    Stub feature bullet component

---
+++ components/FeatureBullet.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="feature section">
+		<div class="container">
+			<h2 class="subtitle has-text-centered"> {{ title }} </h2>
+			<div class="columns" :class="{ 'reverse-row-order': reversed }">
+				<div class="column is-one-third">
+					<div class="image is-square">
+						<slot name="image"></slot>
+					</div>
+				</div>
+				<div class="column">
+					<slot></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: String,
+		reversed: Boolean
+	}
+}
+</script>
+
+<style>
+.reverse-row-order {
+	flex-direction: row-reverse;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <section class="section">
-    <h1>Hello, World!</h1>
-  </section>
+  <div class="index-page">
+  	<section class="section">
+  	  <h1>Hello, World!</h1>
+  	</section>
+  	
+  	<feature-bullet title="The best feature" reversed>
+  		<img slot="image" src="~/assets/img/logo-icon-dark.png" alt="Best feature">
+  		<p>This is the best feature ever</p>
+  	</feature-bullet>
+  </div>
 </template>
 
 <script>
 
+import FeatureBullet from '~/components/FeatureBullet.vue'
+
 export default {
   components: {
+  	FeatureBullet
   }
 }
 </script>
