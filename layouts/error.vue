3710bcf36c74aecfcd781ab27fb3cbc6532b2608
Dutch <dutch@unviral.com>
2018-03-04 20:32:46 -0500

    Add custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="section error">
+		<div class="container has-text-centered">
+			<h1 class="title">Not Found</h1>
+			<p>This page is not currently available. Please double check the URL
+			and try again.</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['error']
+}
+</script>
