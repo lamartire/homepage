59506be58d924832911a0bbf6485566059379f45
Dutch <dutch@unviral.com>
2018-09-05 23:07:24 -0400

    Revert "Stub wallet page"
    This reverts commit d275847cdd4c3869e17128937c59cdcaa2bc2b7b.

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -3,7 +3,6 @@
 		<div class="navbar-start">
 		</div>
 		<div class="navbar-end">
-			<nuxt-link class="navbar-item" to="/wallet" exact>Wallet</nuxt-link>
 			<nuxt-link class="navbar-item" to="/about" exact>About</nuxt-link>
 			<nuxt-link class="navbar-item" to="/careers" exact>Careers</nuxt-link>
 		</div>
