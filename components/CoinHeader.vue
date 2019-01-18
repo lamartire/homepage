f2f75980f77ac54d7f0b3fe0138696c3956c2db0
Dutch <dutch@unviral.com>
2019-01-17 19:26:19 -0500

    Static coin landing page

---
+++ components/CoinHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="coin-header">
+    <hero-banner class="is-light">
+      <div class="hero-inner">
+        <div class="columns">
+          <div class="column">
+            <h1 class="title is-size-2">
+              Free
+              <span class="coin-name">{{name}}</span>
+              <span class="coin-symbol">{{sym}}</span>
+               Web Wallet
+            </h1>
+            <p class="subtitle">Endpass is an open-source, decentralized
+              {{name}} wallet. You can use the Endpass wallet to send or
+              receive <span class="coin-symbol">{{sym}}</span>, or to monitor the <span class="coin-symbol">{{sym}}</span> balance of
+              any Ethereum account address.</p>
+            <p class="subtitle">There's nothing to download and no
+              software to install. Just create a free account to access
+              your <span class="coin-symbol">{{sym}}</span> coins and
+              hundreds of other crypto assets.</p>
+          </div>
+          <div class="column is-one-fifth has-text-centered">
+            <div v-if="logo" class="image coin-logo is-128x128">
+              <img :src="logo" :alt="name" >
+            </div>
+              <a class="button is-medium is-success"
+                 href="https://wallet.endpass.com">{{sym}}
+                 Web Wallet</a>
+          </div>
+        </div>
+      </div>
+      <slot></slot>
+    </hero-banner>
+  </div>
+</template>
+
+<script>
+import HeroBanner from '~/components/HeroBanner.vue'
+
+export default {
+  props: {
+    symbol: String,
+    name: String,
+    logo: String,
+  },
+  computed: {
+    sym() {
+      let symbol = this.symbol || 'token';
+      return symbol.toUpperCase().substring(0,5);
+    }
+  },
+  components: {
+  	HeroBanner,
+  },
+}
+</script>
+
+<style lang="scss">
+.coin-header {
+  .title {
+    span {
+      font-weight: initial;
+    }
+  }
+  .coin-symbol {
+    text-transform: uppercase;
+    font-weight: 700;
+  }
+  .coin-name {
+  }
+  .coin-logo {
+    margin: 1rem auto;
+  }
+}
+</style>
---
+++ pages/coin/binance-coin-bnb.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="coin-page">
+
+    <coin-header
+      :name="name" :symbol="symbol"
+      :logo="logo" >
+    </coin-header>
+
+
+      <div class="section text-section">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <h2 class="subtitle is-size-3 has-text-centered">About {{name}}</h2>
+              <p class="is-size-5">{{name}}, traded with the symbol {{symbol}}
+                is an ERC20 token on the Ethereum blockchain.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="section text-section">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <h2 class="subtitle is-size-3 has-text-centered">{{symbol}}
+                Price and Market Cap</h2>
+              <p class="is-size-5">{{symbol}} is traded on many popular
+                exchanges.</p>
+            </div>
+          </div>
+
+          <div class="columns">
+            <div class="column is-one-half">
+              <div class="coin-price">
+                <script
+src="https://cryptoradarorg-zhxsya288ouz.stackpathdns.com/wp-content/themes/mag-child/inc/widget/widget.js"></script>
+                <div id="cr-widget" data-view="0" :data-coin="name" data-market="1" data-volume="1" data-currency="USD" data-num="1" data-pages="1"></div>
+              </div>
+            </div>
+
+            <div class="column"></div>
+          </div>
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+import CoinHeader from '~/components/CoinHeader.vue'
+
+export default {
+  head () {
+    return {
+      title: `Free ${this.name} ${this.symbol} Wallet`
+    }
+  },
+  data () {
+    return {
+      symbol: "BNB",
+      name: "Binance Coin",
+      logo: "https://tokeninfo.endpass.com/img/0xb8c77482e45f1f44de1745f52c74426c631bdd52.png",
+    }
+  },
+  layout: 'landing',
+  components: {
+    CoinHeader,
+  },
+}
+</script>
+
+
+<style lang="scss">
+.coin-page {
+}
+</style>
