e9869b16eda1a6dbe92a7f6fb8946f98ef73cf37
Dutch <dutch@unviral.com>
2019-07-22 19:22:25 -0400

    start vault page

---
+++ pages/vault.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="careers-page section">
+    <div class="container">
+    	<h1 class="title">Secure Data Vault</h1>
+    	<p class="lead">
+        Protect your personal information and secure your private documents.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+	head () {
+		return {
+			title: 'Secure Data Vault'
+		}
+	}
+}
+</script>
