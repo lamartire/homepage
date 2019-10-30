6b3cf1b1bac1655d4ac1239c7c9cbc59af519c6b
lamartire <lamartire@gmail.com>
2019-10-30 12:53:19 +0300

    fix: some layout issues

---
+++ components/IndexPage/MissionBlock.vue
@@ -0,0 +1,140 @@
+<template>
+  <section class="mission-block">
+    <div class="container mission-block-content">
+      <v-card class="mission-block-card">
+        <div class="columns is-variable is-8">
+          <section
+            class="column is-10 is-offset-1-desktop is-12-touch is-offset-0 mission-block-header"
+          >
+            <div class="mission-block-icon">
+              <v-svg-icon name="feature-6"></v-svg-icon>
+            </div>
+            <h2 class="v-page-subtitle">
+              Our mission against global fraud and cybercriminals
+            </h2>
+            <h3 class="v-page-intro">
+              Fraud and cybercrime are becoming more rampant than ever
+            </h3>
+          </section>
+        </div>
+        <section
+          class="columns is-variable is-8 mission-block-content v-page-content"
+        >
+          <div class="column is-5 is-12-touch is-offset-1-desktop">
+            <p>
+              <strong
+                >We believe better identity verification and more secure data
+                storage practices are the key to defeating fraud.</strong
+              >
+            </p>
+            <p>
+              That’s why we built a digital identity verification platform
+              powered by the most advanced Artificial Intelligence technology to
+              help financial institutions and other businesses ensure their
+              customers are who they say they are.
+            </p>
+          </div>
+          <div class="column is-5 is-12-touch">
+            <p>
+              As our AI reviews documents and detects instances of fraud, it
+              develops “digital fingerprints” that can be used to correlate and
+              eventually pinpoint fraudsters and forgers at their original
+              source, stopping cybercrime in its tracks.
+            </p>
+            <v-more-link href="#"
+              >Join us in the fight against global fraud</v-more-link
+            >
+          </div>
+        </section>
+      </v-card>
+    </div>
+  </section>
+</template>
+
+<script>
+import VSvgIcon from "@endpass/ui/kit/VSvgIcon";
+import VCard from "@endpass/ui/kit/VCard";
+import VMoreLink from "~/components/VMoreLink";
+
+export default {
+  name: "MissionBlock",
+
+  components: {
+    VSvgIcon,
+    VCard,
+    VMoreLink
+  }
+};
+</script>
+
+<style lang="scss">
+.mission-block {
+  padding: 105px 0 120px;
+  background-color: $bg-light-grey;
+  background-image: url(../../assets/img/backgrounds/mission-block.svg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+.mission-block .mission-block-card {
+  padding: 48px 0 70px;
+}
+
+.mission-block-header {
+  text-align: center;
+}
+
+.mission-block-icon {
+  width: 49px;
+  height: 56px;
+  margin: 0 auto 24px;
+}
+
+.mission-block-header h2 {
+  margin-bottom: 8px;
+}
+
+.mission-block-header h3 {
+  margin-bottom: 40px;
+}
+
+.mission-block-content p {
+  margin-bottom: 28px;
+}
+
+@include touch {
+  .mission-block {
+    padding: 0;
+    background-image: none;
+  }
+
+  .mission-block .mission-block-card {
+    padding: 43px 22px 63px;
+  }
+
+  .mission-block-header {
+    text-align: left;
+  }
+
+  .mission-block-icon {
+    margin: 0 0 32px;
+  }
+
+  .mission-block-header h2 {
+    margin-bottom: 8px;
+  }
+
+  .mission-block-header h3 {
+    margin-bottom: 30px;
+  }
+
+  .mission-block-content {
+    padding: 0 !important;
+  }
+
+  .mission-block-content p {
+    margin-bottom: 25px;
+  }
+}
+</style>
---
+++ components/IndexPage/TitleBlock/FeaturesPart.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="container features-part">
+    <v-card class="features-part-card">
+      <div class="columns is-variable is-4">
+        <div class="column is-6 is-12-touch is-offset-1-desktop">
+          <h3 class="v-page-subtitle">
+            How we delight you and your customers:
+          </h3>
+        </div>
+      </div>
+      <ul class="columns is-variable is-4 features-part-list">
+        <li class="column is-3 is-12-touch is-offset-1-desktop is-offset-0">
+          <div class="features-part-icon">
+            <v-svg-icon name="feature-1"></v-svg-icon>
+          </div>
+          <p>
+            Single click KYC verification for seamless customer onboarding
+          </p>
+        </li>
+        <li class="column is-3 is-12-touch">
+          <div class="features-part-icon">
+            <v-svg-icon name="feature-2"></v-svg-icon>
+          </div>
+          <p>Truly automated compliance without confusing setup</p>
+        </li>
+        <li class="column is-3 is-12-touch">
+          <div class="features-part-icon">
+            <v-svg-icon name="feature-3"></v-svg-icon>
+          </div>
+          <p>Superpower your fraud detection with advanced AI</p>
+        </li>
+      </ul>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import VCard from "@endpass/ui/kit/VCard";
+import VSvgIcon from "@endpass/ui/kit/VSvgIcon";
+
+export default {
+  name: "FeaturesPart",
+
+  components: {
+    VCard,
+    VSvgIcon
+  }
+};
+</script>
+
+<style lang="scss">
+.features-part-card {
+  position: relative;
+  z-index: 3;
+  padding: 65px 0;
+}
+
+.features-part-card:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  z-index: 1;
+  right: -1000px;
+  display: block;
+  background: #fff;
+  height: 100%;
+  width: 1000px;
+  box-shadow: inherit;
+}
+
+.features-part-card:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  z-index: 2;
+  right: -5px;
+  display: block;
+  background: #fff;
+  height: 100%;
+  width: 10px;
+}
+
+.features-part-card h3 {
+  margin-bottom: 60px;
+}
+
+.features-part-list {
+  display: flex;
+  align-items: flex-start;
+}
+
+.features-part-list li p {
+  font-size: 20px;
+  line-height: 1.4;
+}
+
+.features-part-icon {
+  width: 46px;
+  height: 46px;
+  margin-bottom: 30px;
+}
+
+@include touch {
+  .container.features-part {
+    padding: 0 !important;
+  }
+
+  .features-part-card h3 {
+    margin-bottom: 40px;
+  }
+
+  .features-part-list li p {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+
+  .features-part .v-card.features-part-card {
+    padding: 44px 23px 55px;
+    border-radius: 0;
+    box-shadow: none;
+  }
+
+  .features-part-list {
+    flex-wrap: wrap;
+  }
+
+  .features-part-list li:not(:last-child) {
+    margin-bottom: 40px;
+  }
+
+  .features-part-icon {
+    margin-bottom: 24px;
+  }
+
+  .features-part-card:before,
+  .features-part-card:after {
+    display: none;
+  }
+}
+</style>
