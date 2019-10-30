f60f9a1921372ff0be414863436506c20c7023cc
lamartire <lamartire@gmail.com>
2019-10-30 12:53:19 +0300

    fix: some layout issues

---
+++ pages/landing.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="landing">
+    <title-block />
+    <summary-block />
+    <single-sign-on-block />
+    <automation-block />
+    <api-block />
+    <mission-block />
+  </div>
+</template>
+
+<script>
+import TitleBlock from "~/components/IndexPage/TitleBlock";
+import SummaryBlock from "~/components/IndexPage/SummaryBlock";
+import ApiBlock from "~/components/IndexPage/ApiBlock.vue";
+import MissionBlock from "~/components/IndexPage/MissionBlock.vue";
+import SingleSignOnBlock from "~/components/IndexPage/SingleSignOnBlock.vue";
+import AutomationBlock from "~/components/IndexPage/AutomationBlock.vue";
+
+export default {
+  name: "LandingPage",
+
+  layout: 'new',
+
+  components: {
+    TitleBlock,
+    SummaryBlock,
+    ApiBlock,
+    MissionBlock,
+    SingleSignOnBlock,
+    AutomationBlock
+  }
+};
+</script>
