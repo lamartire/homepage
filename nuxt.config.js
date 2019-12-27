a7516637be8018eab60b18562312cd4b7ab4cef6
Dutch <dutch@unviral.com>
2019-12-27 17:06:05 -0500

    fix: Title template puts page title first for SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ module.exports = {
    */
   head: {
     title: "automated identity verification",
-    titleTemplate: "Endpass: %s",
+    titleTemplate: "%s | Endpass",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
